7f3d9fabba2e2563d4fbc72a904b16222508826a
rockyroed <roed-imp.vo97b@slmail.me>
2025-10-06 17:27:53 +0800

    fix: fake env variables in testing

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,8 +5,8 @@ export default defineVitestConfig({
     environment: 'nuxt',
     env: {
       NODE_ENV: 'test',
-      NUXT_PUBLIC_CLERK_PUBLISHABLE_KEY: 'pk_test_mock_key_for_testing',
-      NUXT_CLERK_SECRET_KEY: 'sk_test_mock_key_for_testing',
+      NUXT_PUBLIC_CLERK_PUBLISHABLE_KEY: 'pk_test_fake_key',
+      NUXT_CLERK_SECRET_KEY: 'sk_test_fake_key',
     },
   },
 });
