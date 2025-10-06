5517fb78a0c35ce1aad7507c6b7f5675fae61530
rockyroed <roed-imp.vo97b@slmail.me>
2025-10-06 19:47:21 +0800

    feat: create layout

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>
-    <h1 class="text-3xl font-bold underline">Hello World!</h1>
-  </div>
+  <UApp>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </UApp>
 </template>
---
+++ app/layouts/default.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="flex flex-col min-h-screen">
+    <Header />
+    <slot />
+  </div>
+</template>
