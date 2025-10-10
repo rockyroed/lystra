47af6024b7511ab379e1ff0f3ee2a86d7f7bc947
pey2 <fayepabs20@gmail.com>
2025-10-10 22:11:37 +0800

    feat: added multiple search input

---
+++ app/components/MultipleInputMenu/index.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import type { ItemState } from '@/types/item';
+
+const tags = ref(['Placeholder1', 'Placeholder2', 'Placeholder3', 'Placeholder4']);
+const state = reactive<ItemState>({ tags: undefined });
+</script>
+
+<template>
+  <UInputMenu v-model="state.tags" multiple :items="tags" placeholder="Filter Tags" />
+</template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,6 +1,25 @@
 <template>
   <section class="p-5">
-    <Table />
-    <Modal />
+    <div class="flex gap-2 pb-4">
+      <div>
+        <!-- Place search items here -->
+      </div>
+      <div><MultipleInputMenu class="w-90" /></div>
+      <div>
+        <!-- Place Filter Button here -->
+      </div>
+      <div class="flex ml-auto gap-2">
+        <div>
+          <!-- Place Delete Item(s) here -->
+        </div>
+        <div>
+          <!-- Place Add Button here -->
+        </div>
+      </div>
+    </div>
+    <div>
+      <Table />
+      <Modal />
+    </div>
   </section>
 </template>
